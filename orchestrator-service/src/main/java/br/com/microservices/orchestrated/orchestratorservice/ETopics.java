8f575cb4560ee8504ba94ee1d4fa5c51ff95c6ac
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-22 21:12:15 -0300

    criando enums

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/ETopics.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.orchestratorservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author pedroRhamon
+ */
+@AllArgsConstructor
+@Getter
+public enum ETopics {
+
+	START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    NOTIFY_ENDING("notify-ending");
+	
+	private String topic;
+}
