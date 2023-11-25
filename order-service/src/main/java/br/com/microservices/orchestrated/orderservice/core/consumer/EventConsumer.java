77e97822763fc51593e56580b6d8cc962f37dbc8
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-25 00:19:47 -0300

    criando consumer no micro de order

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -0,0 +1,29 @@
+package br.com.microservices.orchestrated.orderservice.core.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author pedroRhamon
+ */
+@Slf4j
+@Component
+@AllArgsConstructor
+public class EventConsumer {
+
+	private final JsonUtil jsonUtil;
+	
+	@KafkaListener(
+			groupId = "${spring.kafka.consumer.group-id}",
+			topics = "${spring.kafka.topic.notify-ending}"
+			)
+	public void consumeNotifyEndingEvent(String payload) {
+		log.info("Receiving ending notification event {} from notify-ending topic", payload);
+		var event = this.jsonUtil.toEvent(payload);
+		log.info(event.toString());
+	}
+}
