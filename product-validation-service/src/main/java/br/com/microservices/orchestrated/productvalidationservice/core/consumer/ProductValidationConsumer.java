cd88c3c493ca84e15f9c4a3597e1e8bd24a6c8d9
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-25 00:25:57 -0300

    implementando logica em outros micro

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/consumer/ProductValidationConsumer.java
@@ -0,0 +1,39 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author pedroRhamon
+ */
+@Slf4j
+@Component
+@AllArgsConstructor
+public class ProductValidationConsumer {
+
+	private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.product-validation-success}"
+    )
+    public void consumeSuccessEvent(String payload) {
+        log.info("Receiving success event {} from product-validation-success topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        productValidationService.validateExistingProducts(event);
+    }
+
+    @KafkaListener(
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.product-validation-fail}"
+    )
+    public void consumeFailEvent(String payload) {
+        log.info("Receiving rollback event {} from product-validation-fail topic", payload);
+        var event = jsonUtil.toEvent(payload);
+        productValidationService.rollbackEvent(event);
+    }
+}
