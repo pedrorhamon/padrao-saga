a797b78aa07c696c3333b2413dedc4401a5b2c2c
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-26 10:31:24 -0300

    criando repositories

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package br.com.microservices.orchestrated.orderservice.core.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Order;
+
+/**
+ * @author pedroRhamon
+ */
+public interface OrderRepository extends MongoRepository<Order, String>{
+
+}
