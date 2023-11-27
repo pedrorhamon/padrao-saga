636dcad0b298371d4bc28c88e4dcec4ad53db884
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-27 20:47:27 -0300

    adicionando anotações

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,8 +1,14 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+@Service
+@RequiredArgsConstructor
 public class EventService {
 
 }
