4449d2177a084cc1ddff2ca5cb84654248d4677f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-29 19:10:19 -0300

    criando service, para manipulacao

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,17 +1,24 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 
 /**
  * @author pedroRhamon
  */
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class EventService {
 	
 	private final EventRepository eventRepository;
+	
+	@Transactional
+	public Event save(Event event) {
+		return this.eventRepository.save(event);
+	}
 
 }
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/OrderService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/OrderService.java
@@ -1,17 +1,54 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.document.Order;
+import br.com.microservices.orchestrated.orderservice.core.dto.OrderRequest;
+import br.com.microservices.orchestrated.orderservice.core.producer.SagaProducer;
+import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import br.com.microservices.orchestrated.orderservice.core.repository.OrderRepository;
-import lombok.RequiredArgsConstructor;
+import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
 
 /**
  * @author pedroRhamon
  */
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class OrderService {
+	
+	private static final String TRANSACTION_ID_PATTERN = "%s_%s";
+	
+	private final JsonUtil jsonUtil;
+	
+	private final SagaProducer sagaProducer;
+	
+	private final EventService eventService;
 	
 	private final OrderRepository orderRepository;
 
+	public Order createOrder(OrderRequest orderRequest) {
+		var order = Order.builder()
+				.products(orderRequest.getProducts())
+				.createdAt(LocalDateTime.now())
+				.transactionId(String.format(TRANSACTION_ID_PATTERN, Instant.now().toEpochMilli(), UUID.randomUUID()))
+				.build();
+		
+		return this.orderRepository.save(order);
+	}
+	
+	private Event createPayload(Order order) {
+		var event = Event.builder()
+				.orderId(order.getId())
+				.transactionId(order.getTransactionId())
+				.playload(order)
+				.createdAt(LocalDateTime.now())	
+				.build();
+		return this.eventService.save(event);
+	}
 }
