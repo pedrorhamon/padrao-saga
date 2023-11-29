7442b7abea694780870d943fc57f9b49d94b58f0
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-11-28 23:16:03 -0300

    adicionando o repository no service

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -2,6 +2,7 @@ package br.com.microservices.orchestrated.orderservice.core.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -10,5 +11,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class EventService {
+	
+	private final EventRepository eventRepository;
 
 }
