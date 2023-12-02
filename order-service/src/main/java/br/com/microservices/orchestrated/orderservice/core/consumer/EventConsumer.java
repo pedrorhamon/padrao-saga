c79f5cff1729db6b87801aed3fd9cd1d3d2cfda0
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-02 20:31:52 -0300

    implementando o consumer

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -3,6 +3,7 @@ package br.com.microservices.orchestrated.orderservice.core.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,8 @@ public class EventConsumer {
 
 	private final JsonUtil jsonUtil;
 	
+	private final EventService eventService;
+	
 	@KafkaListener(
 			groupId = "${spring.kafka.consumer.group-id}",
 			topics = "${spring.kafka.topic.notify-ending}"
@@ -24,6 +27,6 @@ public class EventConsumer {
 	public void consumeNotifyEndingEvent(String payload) {
 		log.info("Receiving ending notification event {} from notify-ending topic", payload);
 		var event = this.jsonUtil.toEvent(payload);
-		log.info(event.toString());
+		this.eventService.notifyEnding(event);
 	}
 }
