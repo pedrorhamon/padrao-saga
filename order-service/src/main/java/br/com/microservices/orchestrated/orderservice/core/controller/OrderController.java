a37376e2789ab5713d794cadb76cf1c371eceb85
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-02 20:33:06 -0300

    criando controller

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/OrderController.java
@@ -0,0 +1,17 @@
+package br.com.microservices.orchestrated.orderservice.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+
+}
