055a3d6b7d49a557185cf9eb1807a249291b081e
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-03 19:54:04 -0300

    createOrder

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/OrderController.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/controller/OrderController.java
@@ -1,9 +1,12 @@
 package br.com.microservices.orchestrated.orderservice.core.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.microservices.orchestrated.orderservice.core.document.Order;
+import br.com.microservices.orchestrated.orderservice.core.dto.OrderRequest;
 import br.com.microservices.orchestrated.orderservice.core.service.OrderService;
 import lombok.AllArgsConstructor;
 
@@ -18,8 +21,9 @@ public class OrderController {
 	
 	private final OrderService orderService;
 	
-	public ResponseEntity<?> saveOrder() {
-		
+	@PostMapping
+	public Order createOrder(@RequestBody OrderRequest orderRequest) {
+		return this.orderService.createOrder(orderRequest);
 	}
 
 }
