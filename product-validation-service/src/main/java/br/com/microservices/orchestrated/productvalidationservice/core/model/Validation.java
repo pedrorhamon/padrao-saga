a8051dccff948315bc5f107755d15fc4433bf0d4
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-04 19:17:47 -0300

    adicionando atributos na class entity Validation

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
@@ -1,8 +1,33 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "validation")
 public class Validation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String orderId;
+	
+	private String transactionId;
+	
+	private boolean success;
 
 }
