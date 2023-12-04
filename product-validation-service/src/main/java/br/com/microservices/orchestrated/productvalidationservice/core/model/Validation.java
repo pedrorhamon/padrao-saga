0bf4fe8e4c70562ba619063a580267a8961f0df2
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-04 19:23:04 -0300

    concluindo entidade Validation

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
@@ -7,8 +7,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,29 +20,42 @@ import lombok.NoArgsConstructor;
  */
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "validation")
 public class Validation {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(nullable = false)
 	private String orderId;
-	
+
 	@Column(nullable = false)
 	private String transactionId;
-	
+
 	@Column(nullable = false)
 	private boolean success;
-	
-	@Column(nullable = false)
+
+	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
-	
+
 	@Column(nullable = false)
 	private LocalDateTime updateAt;
+
+	@PrePersist
+	public void prePersist() {
+		var now = LocalDateTime.now();
+		createdAt = now;
+		updateAt = now;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = LocalDateTime.now();
+	}
 
 }
