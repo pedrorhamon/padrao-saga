539a9f2e4ea0acc53daf4cf21f9f6690ea0576e2
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-04 19:13:19 -0300

    adicionando anotaçoes

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Product.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Product.java
@@ -1,8 +1,28 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String code;
 }
