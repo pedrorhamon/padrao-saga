a374875929f3676f591b05926ab6fc28b90223c2
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-09 01:22:04 -0300

    criando metodo para validation

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/service/ProductValidationService.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/service/ProductValidationService.java
@@ -1,7 +1,9 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.service;
 
 import org.springframework.stereotype.Service;
+import static org.springframework.util.ObjectUtils.isEmpty;
 
+import br.com.microservices.orchestrated.productvalidationservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.productvalidationservice.core.dto.Event;
 import br.com.microservices.orchestrated.productvalidationservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.productvalidationservice.core.repository.ProductRepository;
@@ -36,6 +38,7 @@ public class ProductValidationService {
 			this.handleSuccess(event);
 		} catch (Exception e) {
 			log.error("Error trying to validation products: ", e);
+			this.handleFailCurrentNotExecuted(event, e.getMessage());
 		}
 		
 		this.producer.sendEvent(this.jsonUtil.toJson(event));
@@ -52,8 +55,16 @@ public class ProductValidationService {
 	}
 
 	private void checkCurrentValidation(Event event) {
-		// TODO Auto-generated method stub
-		
+		this.validateProductsInformed(event);
+	}
+
+	private void validateProductsInformed(Event event) {
+		if(isEmpty(event.getPlayload().getId()) || isEmpty(event.getPlayload().getProducts())) {
+			throw new ValidationException("Product List is empty!");
+		}
+		if(isEmpty(event.getPlayload()) || isEmpty(event.getPlayload().getProducts())) {
+			throw new ValidationException("OrderID and TransactionID must be informed!");
+		}
 	}
 
 }
