cf50803dcd6bb8c65c84ca2fcb51aec39e5253c8
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-08 23:24:59 -0300

    ajustes

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -13,8 +13,6 @@ import br.com.microservices.orchestrated.orderservice.core.document.Event;
 public interface EventRepository extends MongoRepository<Event, String> {
 
 	List<Event> findAllByOrderByCreatedAtDesc();
-
 	Optional<Event> findTop1ByOrderIdOrderByCreatedAtDesc(String orderId);
-
 	Optional<Event> findTop1ByTransactionIdOrderByCreatedAtDesc(String transactionId);
 }
