3dc556cca04b813f5dcb14d45ed29e3e1eb6e2f6
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-12 20:41:16 -0300

    criando package

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/ProductValidationController.java
+++
@@ -1,8 +0,0 @@
-package br.com.microservices.orchestrated.productvalidationservice.core;
-
-/**
- * @author pedroRhamon
- */
-public class ProductValidationController {
-
-}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/controllers/ProductValidationController.java
@@ -0,0 +1,8 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.controllers;
+
+/**
+ * @author pedroRhamon
+ */
+public class ProductValidationController {
+
+}
