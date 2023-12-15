50d1e6afa78e586edbfd6cefe30d4db89b1b1674
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-15 20:33:35 -0300

    instanciando repository no service

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -1,8 +1,22 @@
 package br.com.microservices.orchestrated.paymentservice.core.service;
 
+import org.springframework.stereotype.Service;
+
+import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author pedroRhamon
  */
+@Slf4j
+@Service
+@AllArgsConstructor
 public class PaymentService {
+	
+    private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
+	
+	private final PaymentRepository paymentRepository;
 
+	
 }
