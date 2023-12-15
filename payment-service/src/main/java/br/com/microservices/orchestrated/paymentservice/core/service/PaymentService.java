50e87acfbadf9461c05f873f72cff987fdb5729a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-15 20:43:01 -0300

    created realizePayment()

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -2,7 +2,10 @@ package br.com.microservices.orchestrated.paymentservice.core.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRepository;
+import br.com.microservices.orchestrated.paymentservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,10 +16,21 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @AllArgsConstructor
 public class PaymentService {
-	
-    private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
-	
+
+	private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
+
 	private final PaymentRepository paymentRepository;
 
+	private final JsonUtil jsonUtil;
+	private final KafkaProducer producer;
 	
+	public void realizePayment(Event event) {
+		try {
+			
+		} catch (Exception e) {
+            log.error("Error trying to make payment: ", e);
+		}
+		this.producer.sendEvent(this.jsonUtil.toJson(event));
+	}
+
 }
