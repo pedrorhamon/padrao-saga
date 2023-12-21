9475d25a5acb7dcd41ae0a08c083896a7b760e9f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-21 19:19:25 -0300

    adicionando anotações

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -1,7 +1,5 @@
 package br.com.microservices.orchestrated.inventoryservice.core.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +10,9 @@ import lombok.NoArgsConstructor;
  */
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "inventory")
-public class Inventory {
+@Builder
+public class OrderInventory {
 
 }
