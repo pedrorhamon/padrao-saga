abfa88759b8f4feaaf7fced9884165365b391776
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-21 19:24:42 -0300

    adicionando atributos na class

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -1,7 +1,18 @@
 package br.com.microservices.orchestrated.inventoryservice.core.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +23,40 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Entity
+@Table(name = "order_inventory")
 public class OrderInventory {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "inventory_id", nullable = false)
+	private Inventory inventory;
+
+	@Column(nullable = false)
+	private String orderId;
+
+	@Column(nullable = false)
+	private String transactionId;
+	
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@Column(nullable = false)
+	private LocalDateTime updateAt;
+	
+	@PrePersist
+	public void prePersist() {
+		var now = LocalDateTime.now();
+		createdAt = now;
+		updateAt = now;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = LocalDateTime.now();
+	}
 
 }
