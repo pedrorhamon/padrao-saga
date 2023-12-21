ea578704315b59c6c1b388d7a63290bf3af9ff51
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-21 19:17:49 -0300

    adicionando anotações

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/Inventory.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/Inventory.java
@@ -1,8 +1,18 @@
 package br.com.microservices.orchestrated.inventoryservice.core.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Inventory {
 
 }
