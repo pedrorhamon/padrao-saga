1612c41d0bbca5a5a2887353e6d2703f747f71b4
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-22 22:31:10 -0300

    adicionando @Service

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -1,8 +1,12 @@
 package br.com.microservices.orchestrated.inventoryservice.core.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author pedroRhamon
  */
+
+@Service
 public class InventoryService {
 
 }
