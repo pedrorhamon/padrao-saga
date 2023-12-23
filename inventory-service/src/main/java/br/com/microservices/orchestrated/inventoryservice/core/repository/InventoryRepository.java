29bf608e0637bcddafa12c67af25f0239bc6189c
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-22 22:32:35 -0300

    criando no repository query

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/repository/InventoryRepository.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/repository/InventoryRepository.java
@@ -1,5 +1,7 @@
 package br.com.microservices.orchestrated.inventoryservice.core.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.microservices.orchestrated.inventoryservice.core.model.Inventory;
@@ -8,5 +10,7 @@ import br.com.microservices.orchestrated.inventoryservice.core.model.Inventory;
  * @author pedroRhamon
  */
 public interface InventoryRepository extends JpaRepository<Inventory, Integer>{
+	
+	Optional<Inventory> findByProductCode(String productCode);
 
 }
