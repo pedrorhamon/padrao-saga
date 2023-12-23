fbe79d6b7df987e83396f15529aa16f846e3421d
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-23 06:55:27 -0300

    instanciando repository

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -2,11 +2,17 @@ package br.com.microservices.orchestrated.inventoryservice.core.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.inventoryservice.core.repository.InventoryRepository;
+import lombok.AllArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
 
 @Service
+@AllArgsConstructor
 public class InventoryService {
+	
+	private final InventoryRepository inventoryRepository;
 
 }
