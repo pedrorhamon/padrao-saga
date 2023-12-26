7c8c5741afd9da139fda13f57f1d3774bd59ed6d
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-26 20:06:08 -0300

    adicionando na class service repository, comecar a colocar regras

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -3,7 +3,10 @@ package br.com.microservices.orchestrated.inventoryservice.core.service;
 import org.springframework.stereotype.Service;
 
 import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import br.com.microservices.orchestrated.inventoryservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.inventoryservice.core.repository.InventoryRepository;
+import br.com.microservices.orchestrated.inventoryservice.core.repository.OrderInventoryRepository;
+import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 
 /**
@@ -13,19 +16,24 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class InventoryService {
-	
+
+	private static final String CURRENT_SOURCE = "INVENTORY_SERVICE";
+
+	private final JsonUtil jsonUtil;
+	private final KafkaProducer producer;
 	private final InventoryRepository inventoryRepository;
+	private final OrderInventoryRepository orderInventoryRepository;
 
 	public void updateInventory(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void rollbackInventory(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	private void changeValidationToFail(Event event) {
 	}
 
