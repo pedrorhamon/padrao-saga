c930ebdedb7a37a8fc6c52dcf594e5865cf5fb02
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-12-27 09:01:09 -0300

    iniciando service e implementacoes

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -2,17 +2,19 @@ package br.com.microservices.orchestrated.inventoryservice.core.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.inventoryservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
 import br.com.microservices.orchestrated.inventoryservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.inventoryservice.core.repository.InventoryRepository;
 import br.com.microservices.orchestrated.inventoryservice.core.repository.OrderInventoryRepository;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author pedroRhamon
  */
-
+@Slf4j
 @Service
 @AllArgsConstructor
 public class InventoryService {
@@ -25,8 +27,18 @@ public class InventoryService {
 	private final OrderInventoryRepository orderInventoryRepository;
 
 	public void updateInventory(Event event) {
-		// TODO Auto-generated method stub
+		try {
+			this.checkCurrentValidation(event);
 
+		} catch (Exception e) {
+			log.error("Error trying to update inventory: ", e);
+		}
+	}
+
+	private void checkCurrentValidation(Event event) {
+		if (this.orderInventoryRepository.existsByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId())) {
+			throw new ValidationException("There's another transactionId for this validation.");
+		}
 	}
 
 	public void rollbackInventory(Event event) {
