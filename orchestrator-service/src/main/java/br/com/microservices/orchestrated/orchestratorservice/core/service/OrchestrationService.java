6aef48e17aba05eedd931361143834c1c421b6f7
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-01-06 12:42:30 -0300

    adicionando anotacoes

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/service/OrchestrationService.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/service/OrchestrationService.java
@@ -1,8 +1,15 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+
+@Service
+@AllArgsConstructor
 public class OrchestrationService {
 
 }
