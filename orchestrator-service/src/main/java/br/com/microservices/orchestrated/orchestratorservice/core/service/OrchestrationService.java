7549f4a865944227f9c51b5cce012109cddb1b7a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-01-06 12:43:21 -0300

    instanciando o produce e o jsonUtil

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/service/OrchestrationService.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/service/OrchestrationService.java
@@ -2,14 +2,21 @@ package br.com.microservices.orchestrated.orchestratorservice.core.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.orchestratorservice.core.producer.SagaOrchestratorProducer;
+import br.com.microservices.orchestrated.orchestratorservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author pedroRhamon
  */
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class OrchestrationService {
+	
+	private final SagaOrchestratorProducer producer;
+    private final JsonUtil jsonUtil;
 
 }
