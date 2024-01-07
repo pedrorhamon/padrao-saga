744cd6a3e1112a718fc3bb8ddda03746f820cccf
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-01-07 17:19:15 -0300

    consertando atributos

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Event.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/dto/Event.java
@@ -23,7 +23,7 @@ public class Event {
 	private String id;
 	private String transactionId;
 	private String orderId;
-	private Order playload;
+	private Order payload;
 	private EEventSource source;
 	private ESagaStatus status;
 	private List<History> eventHistory;
